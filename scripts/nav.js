4b9be6d920b5021f108b5d57d2a978d83265105b
Morgan Vo <morganvo@outlook.com>
2020-11-11 17:51:07 -0500

    integrated mobile menu

---
+++ scripts/nav.js
@@ -0,0 +1,27 @@
+// nav.js controls opened/closed states of mobile navigation
+
+function initNav() {
+  const nav = document.querySelector('nav'),
+    toggle = document.querySelector('.nav-toggle');
+
+  let active = false;
+
+  function closeMenu () {
+    nav.classList.remove('nav--active');
+    toggle.innerHTML = '(menu)';
+    active = false;
+  }
+
+  toggle.addEventListener('click', () => {
+    // closed state
+    if (active) {
+      closeMenu();
+    }
+    // open state
+    else {
+      toggle.innerHTML = '(close menu)';
+      nav.classList.add('nav--active');
+      active = true;
+    }
+  });
+}
